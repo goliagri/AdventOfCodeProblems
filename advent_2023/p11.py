3e25c41571fe3badb7cca9f3ea8a1ff54e373124
Griffin <griffingolias@gmail.com>
2023-12-24 12:16:02 -0800

    finished p11

--- advent_2023/p11.py
+++ advent_2023/p11.py
@@ -1,8 +1,43 @@
 #https://adventofcode.com/2023/day/11
 input_path = 'advent_2023_data/'
 
+class SegmentTreeNode:
 
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+        self.val = 0
+        self.lazy = 0
 
+        if start != end:
+            mid = (start + end) // 2
+            self.left = SegmentTreeNode(start, mid)
+            self.right = SegmentTreeNode(mid+1, end)
+    
+    def update(self, start, end, val):
+        if start > self.end or end < self.start:
+            return
+        elif start <= self.start and end >= self.end:
+            self.val += val * (self.end - self.start + 1)
+            self.lazy += val
+            return
+        else:
+            self.left.update(start, end, val)
+            self.right.update(start, end, val)
+            self.val = self.left.val + self.right.val + self.lazy * (self.end - self.start + 1)
+
+    def query(self, start, end):
+        if start > self.end or end < self.start:
+            return 0
+        if start <= self.start and end >= self.end:
+            return self.val
+        
+        if self.lazy != 0:
+            self.left.update(self.lazy)
+            self.right.update(self.lazy)
+            self.lazy = 0
+
+        return self.left.query(start, end) + self.right.query(start, end)
 
 
 
@@ -16,72 +51,51 @@ def sum_shortest_galactic_paths(file, pt):
     cols_with_galaxy = set()
     rows_with_galaxy = set()
 
-    if pt == 2:
-        added_cols = set()
-        added_rows = set()
-
     for i, line in enumerate(lines):
         for j, char in enumerate(line):
             if char == '#':
                 cols_with_galaxy.add(j)
                 rows_with_galaxy.add(i) 
-    print(cols_with_galaxy, rows_with_galaxy)
 
-    for line in lines:
-        print(line)
-    offset = 0
+
+    col_expansions = SegmentTreeNode(0, len(lines[0]))
     for i in range(len(lines[0])):
-        if i not in cols_with_galaxy:
-            lines = [line[:i+offset] + '.' + line[i+offset:] for line in lines]
-            if pt == 2:
-                added_cols.add(i+offset)
-            offset += 1
-    print('-----------------------------------------------')
-    offset = 0
+        if not i in cols_with_galaxy:
+            col_expansions.update(i, i, 1)
+
+    row_expansions = SegmentTreeNode(0, len(lines))
     for i in range(len(lines)):
-        if i not in rows_with_galaxy:
-            lines.insert(i+offset, '.'*len(lines[0]))
-            if pt == 2:
-                added_rows.add(i+offset)
-            offset += 1
+        if not i in rows_with_galaxy:
+            row_expansions.update(i, i, 1)
 
-    for line in lines:
-        print(line)
+    if pt == 1:
+        expansion_factor = 2
+    elif pt == 2:
+        expansion_factor = 1000000
+
+    galaxies = []
+    for i in range(len(lines)):
+        for j in range(len(lines[0])):
+            if lines[i][j] == '#':
+                galaxies.append((i,j))
     
-    galaxy_coords = set()
-
-    for i, line in enumerate(lines):
-        for j, char in enumerate(line):
-            if char == '#':
-                galaxy_coords.add((i,j))
-
-    galaxy_coords = list(galaxy_coords)
-
     res = 0
-    for i, galaxy_1 in enumerate(galaxy_coords):
-        for galaxy_2 in galaxy_coords[i+1:]:
-            res += abs(galaxy_1[0] - galaxy_2[0]) + abs(galaxy_1[1] - galaxy_2[1])
-            if pt == 2:
-                ADDED_SPACE_COST = 100 - 1
-                lower_x = min(galaxy_1[0], galaxy_2[0])
-                upper_x = max(galaxy_1[0], galaxy_2[0])
-                for x in range(lower_x+1, upper_x):
-                    if x in added_cols:
-                        res += ADDED_SPACE_COST - 1
-                lower_y = min(galaxy_1[1], galaxy_2[1])
-                upper_y = max(galaxy_1[1], galaxy_2[1])
-                for y in range(lower_y+1, upper_y):
-                    if y in added_rows:
-                        res += ADDED_SPACE_COST - 1
-    
-    return res
-    
+    for i, galaxy_1 in enumerate(galaxies):
+        for j, galaxy_2 in enumerate(galaxies[i+1:]):
+            lower_x = min(galaxy_1[0], galaxy_2[0])
+            upper_x = max(galaxy_1[0], galaxy_2[0])
+            lower_y = min(galaxy_1[1], galaxy_2[1])
+            upper_y = max(galaxy_1[1], galaxy_2[1])
+            res += upper_x - lower_x + upper_y - lower_y #manhattan distance
+            res += col_expansions.query(lower_y, upper_y) * (expansion_factor - 1)
+            res += row_expansions.query(lower_x, upper_x) * (expansion_factor - 1)
 
+    return res
 
 
 def main():
     pt = 2
-    print(sum_shortest_galactic_paths(input_path + 'p11_example.txt', pt))
+    print(sum_shortest_galactic_paths(input_path + 'p11_data.txt', pt))
 
 
 if __name__ == '__main__':
