801e4a95477421bd90246cf5896bd495164571f6
Griffin <griffingolias@gmail.com>
2023-12-25 10:52:03 -0800

    p13 done

--- advent_2023/p13.py
+++ advent_2023/p13.py
@@ -4,29 +4,47 @@ input_path = 'advent_2023_data/'
 def score_surface(surface, pt):
     HORIZONTAL_SCORE_MULTIPLIER = 1
     VERTICAL_SCORE_MULTIPLIER = 100
+    NUM_SMUDGES = 1
     #check for vertical symmetry
-    for i in range(1,len(surface[0])-1):
-        for j in range(len(surface)):
+    for i in range(1,len(surface[0])):
+        if pt == 1:
             is_symm = True
-            for k in range(1, min(i+1, len(surface[0])-i)):
-                if surface[j][i-k] != surface[j][i+k]:
-                    is_symm = False
-                    break
-        if is_symm:
-            print('!{}!'.format(i))
+        elif pt == 2:
+            symm_breakers = 0
+        for j in range(len(surface)):
+            for k in range(0, min(i, len(surface[0])-i)):
+                if surface[j][i-1-k] != surface[j][i+k]:
+                    if pt == 1:
+                        is_symm = False
+                        break
+                    elif pt == 2:
+                        symm_breakers += 1
+                        if symm_breakers > NUM_SMUDGES:
+                            break
+        if (pt == 1 and is_symm) or (pt == 2 and symm_breakers == NUM_SMUDGES):
             return i * HORIZONTAL_SCORE_MULTIPLIER
 
     #check for horizontal symmetry
-    for j in range(1,len(surface)-1):
-        for i in range(len(surface[0])):
+    for j in range(1,len(surface)):
+        if pt == 1:
             is_symm = True
-            for k in range(1, min(j+1, len(surface)-j)):
-                if surface[j-k][i] != surface[j+k][i]:
-                    is_symm = False
-                    break
-        if is_symm:
-            print('!')
+        elif pt == 2:
+            symm_breakers = 0
+        for i in range(len(surface[0])):
+            for k in range(0, min(j, len(surface)-j)):
+                if surface[j-1-k][i] != surface[j+k][i]:
+                    if pt == 1:
+                        is_symm = False
+                        break
+                    elif pt == 2:
+                        symm_breakers += 1
+                        if symm_breakers > NUM_SMUDGES:
+                            break
+        if (pt == 1 and is_symm) or (pt == 2 and symm_breakers == NUM_SMUDGES):
             return j * VERTICAL_SCORE_MULTIPLIER
+    for line in surface:
+        print(line)
+    raise Exception('No symmetry found')
 
 def find_and_score_mirrors(file, pt):
     f = open(file, 'r')
@@ -52,8 +70,8 @@ def find_and_score_mirrors(file, pt):
 
 
 def main():
-    pt = 1
-    print(find_and_score_mirrors(input_path + 'p13_example.txt', pt))
+    pt = 2
+    print(find_and_score_mirrors(input_path + 'p13_data.txt', pt))
 
 
 
